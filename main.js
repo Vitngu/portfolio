75834358c0f5573ac0861b89c27f015f5ee3ce26
Vitngu <enda.wannafly@gmail.com>
2024-12-31 16:37:27 +0700

    lazy loading

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Cao Hai Dang's photography portfolio site");
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('img');
+  
+    images.forEach(image => {
+      if (!image.hasAttribute('loading')) {
+        image.setAttribute('loading', 'lazy');
+      }
+    });
+  });
+
+  
