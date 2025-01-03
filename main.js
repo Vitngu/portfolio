8a9fd760d547313cb8569dca88917be3c01d8396
Vitngu <enda.wannafly@gmail.com>
2025-01-03 14:54:45 +0700

    ©

--- main.js
+++ main.js
@@ -8,4 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('img:not([alt])'); // Chỉ chọn ảnh chưa có alt
+    const defaultAltText = "Mô tả hình ảnh"; // Nội dung alt mặc định
   
+    images.forEach(image => {
+      image.setAttribute('alt', "© Cao Hải Đăng");
+    });
+  });
