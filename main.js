e2259e1683c711464093f354253ef3f276f3e8e4
Vitngu <enda.wannafly@gmail.com>
2025-01-03 15:18:40 +0700

    currentyear auto update

--- main.js
+++ main.js
@@ -8,11 +8,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('img:not([alt])');
     const AltText = "© Cao Hải Đăng";
   
     images.forEach(image => {
       image.setAttribute('alt', AltText);
     });
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const copyrightFooter = document.getElementById('copyrightFooter');
+  
+    if (copyrightFooter) {
+      const currentYear = new Date().getFullYear();
+      copyrightFooter.textContent = "© " + currentYear + " Cao Hải Đăng";
+    }
   });
